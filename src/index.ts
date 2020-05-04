7a3072eeacc10b4ee4e7abf050f1740ee18a3556
Dmytro Hachok <d.gachok@gmail.com>
2020-05-05 00:52:47 +0200

    update webpack config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import fs from 'fs';
 import chalk from 'chalk';
 import readlineSync from 'readline-sync';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
+const NodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'production',
@@ -10,9 +11,10 @@ module.exports = {
     path: __dirname,
     filename: '[name].js',
   },
+  target: 'node',
   resolve: {
     modules: [path.resolve(__dirname, './'), 'node_modules'],
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   optimization: {
     minimize: true,
@@ -20,9 +22,10 @@ module.exports = {
   module: {
     rules: [{ test: /\.ts?$/, loader: 'ts-loader' }],
   },
-  plugins: [ new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }), ],
+  plugins: [new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })],
   node: {
     fs: 'empty',
-    child_process: 'empty'
+    child_process: 'empty',
   },
+  externals: [NodeExternals()],
 };
